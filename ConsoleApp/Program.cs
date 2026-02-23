1fcde64c7e6e6378559277de89ec1a3e06de9e10
Paweł <pawel@msd.info.pl>
2026-02-23 11:01:47 +0100

    ImplicitUsings

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿//instrukcje najwyższego poziomu - top-level statements
+﻿
+//instrukcje najwyższego poziomu - top-level statements
 //są to instrukcje, które można umieścić bezpośrednio w przestrzeni nazw, bez konieczności definiowania klasy i metody Main. Kompilator automatycznie generuje klasę i metodę Main, która jest punktem wejścia do programu. Dzięki temu kod staje się bardziej zwięzły i czytelny, zwłaszcza dla prostych programów.
 //Wszystko co znajduje się w pliku z top-level statements jest otoczone metodą Main
 Console.WriteLine("Hello, World!");
