e34f5b73756a52efb80e3c4ec36184cef88ef8aa
Paweł <pawel@msd.info.pl>
2026-02-24 12:44:57 +0100

    ItemsManager: Edit (TryPattern, parametr opcjonalny)

--- ItemsManager/Program.cs
+++ ItemsManager/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Services.Interfaces;
-using System.Reflection.Emit;
 
 
 /*Song song1 = new Song() { Title = "Song1", Artist = "Artist1" };
@@ -30,7 +29,7 @@ while (!exit)
         Console.WriteLine($"{product.Id} - {product.Name} - {product.Price} - {product.CreatedAt}");
     }
 
-    Console.WriteLine("Commands: create, delete, exit");
+    Console.WriteLine("Commands: create, edit, delete, exit");
 
     string input = Console.ReadLine()!;
 
@@ -38,6 +37,9 @@ while (!exit)
     {
         case "create":
             Create();
+            break;
+        case "edit":
+            Edit();
             break;
         case "delete":
             Delete();
@@ -54,14 +56,39 @@ while (!exit)
     Console.ReadKey();
 }
 
+void Edit()
+{
+    int id = ReadInt("Id: ");
+    var entity = service.Read(id);
+    if (entity == null)
+    {
+        Console.WriteLine("Id not found");
+        return;
+    }
+
+    var newEntity = new Models.Product
+    {
+        Name = ReadString($"Name ({entity.Name}): ", entity.Name),
+        Price = ReadFloat($"Price ({entity.Price}): ", entity.Price),
+        CreatedAt = ReadDate($"Created at ({entity.CreatedAt}): ", entity.CreatedAt)
+    };
+
+
+    service.Update(id, newEntity);
+}
+
+
 void Create()
 {
     var entity = new Models.Product();
     entity.Name = ReadString("Name: ");
+    entity.Price = ReadFloat("Price: ");
     entity.CreatedAt = ReadDate("Created at: ");
+
 
     service.Create(entity);
 }
+
 
 void Delete()
 {
@@ -85,12 +112,17 @@ void Delete()
 
     if (!service.Delete(id))
         Console.WriteLine("Id not found");
+
+
 }
 
-static DateTime ReadDate(string label)
+static DateTime ReadDate(string label, DateTime @default = default)
 {
     Console.Write(label);
     var input = Console.ReadLine()!;
+    if(string.IsNullOrWhiteSpace(input))
+        return @default;
+
     DateTime dateTime;
     try
     {
@@ -118,11 +150,78 @@ static DateTime ReadDate(string label)
         dateTime = ReadDate(label);
     }
 
+
     return dateTime;
 }
 
-static string ReadString(string label)
+/*string ReadString(string label)
 {
     Console.Write(label);
     return Console.ReadLine()!;
+}*/
+
+//@ - pozwala używać słów kluczowych jako nazw zmiennych, parametrów itp. - przydatne gdy chcemy zachować czytelność kodu i użyć słowa kluczowego jako nazwy
+//parametr domyślny - pozwala na wywołanie metody bez podawania wartości dla tego parametru, jeśli nie jest on wymagany. Dziala tak jakby metoda była przeciążona o wersję bez tego parametru
+string ReadString(string label, string @default = "")
+{
+    Console.Write(label);
+    var @string = Console.ReadLine()!;
+    if (string.IsNullOrWhiteSpace(@string))
+    {
+        return @default;
+    }
+    return @string;
+}
+
+float ReadFloat(string label, float @default = 0)
+{
+    Console.Write(label);
+    var input = Console.ReadLine()!;
+
+    if(string.IsNullOrWhiteSpace(input))
+        return @default;
+
+    float value;
+    //tryParse - próbuje przekonwertować string na wskazany typ, jeśli się nie uda to nie rzuca wyjątkiem tylko zwraca false
+    //wartość przekonwertowana jest zwracana przez parametr out
+    bool success = float.TryParse(input, out value);
+
+    if (success)
+    {
+        return value;
+    }
+    Console.WriteLine("Invalid price");
+    return ReadFloat(label);
+}
+
+
+int ReadInt(string label)
+{
+    int value = 4;
+    if (!TryReadInt(label, out value))
+    {
+        ReadInt(label);
+    }
+    return value;
+}
+
+
+//worzymy własną metodę zgodnie z Try Pattern - metoda, która próbuje wykonać jakąś operację i zwraca boola informującego o sukcesie oraz wynik tej operacji przez parametr out
+bool TryReadInt(string label, out int result)
+{
+    Console.Write(label);
+    string input = Console.ReadLine()!;
+
+    try
+    {
+        result = int.Parse(input);
+
+        return true;
+    }
+    catch
+    {
+        Console.WriteLine("Invalid number format");
+        result = default;
+        return false;
+    }
 }
