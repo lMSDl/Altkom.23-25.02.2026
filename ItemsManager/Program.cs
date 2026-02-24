fc1c7bb54cd838f56cf79146686eac5bbaaf9f6a
Paweł <pawel@msd.info.pl>
2026-02-24 11:18:43 +0100

    ItemsManager: Delete (try-catch)

--- ItemsManager/Program.cs
+++ ItemsManager/Program.cs
@@ -1,9 +1,7 @@
-﻿using ItemsManager;
-using Services.InMemory;
-using Services.Interfaces;
+﻿using Services.Interfaces;
 
 
-Song song1 = new Song() { Title = "Song1", Artist = "Artist1" };
+/*Song song1 = new Song() { Title = "Song1", Artist = "Artist1" };
 Song song2 = new Song() { Title = "Song2", Artist = "Artist2" };
 
 Podcast podcast1 = new Podcast { Title = "Podcast", EpisodNumber = 1 };
@@ -14,15 +12,63 @@ IEnumerable<IPlayable> playables = new List<IPlayable> { podcast1, podcast2, son
 foreach (var playable in playables)
 {
     Player.PlayItem(playable);
-}
+}*/
 
 
 IProductsService service = new Services.InMemory.ProductsService();
-
 service.Create(new Models.Product() { Name = "Laptop", Price = 2000 });
 service.Create(new Models.Product() { Name = "Czajnik", Price = 100 });
 
-foreach (var product in service.ReadAll())
+bool exit = false;
+
+while (!exit)
 {
-    Console.WriteLine($"{product.Id} - {product.Name} - {product.Price}");
+    Console.Clear();
+    foreach (var product in service.ReadAll())
+    {
+        Console.WriteLine($"{product.Id} - {product.Name} - {product.Price}");
+    }
+
+    Console.WriteLine("Commands: delete, exit");
+
+    string input = Console.ReadLine()!;
+
+    switch (input.ToLower())
+    {
+        case "delete":
+            Delete();
+            break;
+        case "exit":
+            exit = true;
+            break;
+        default:
+            Console.WriteLine("Unknown command");
+            break;
+    }
+
+    Console.WriteLine("Press any button..");
+    Console.ReadKey();
+}
+void Delete()
+{
+    string input;
+    int id;
+
+    Console.Write("Id: ");
+    input = Console.ReadLine()!;
+
+    //try-catch - służy do obsługi wyjątków
+    //w bloku try umieszczamy kod, który może rzucić wyjątek
+    try
+    {
+        id = int.Parse(input);
+    }
+    //catch (bez parametrów) - przechwytujemy wszystkie wyjątki i nie dostajemy informacji jaki to wyjątek
+    catch
+    {
+        id = 0;
+    }
+
+    if (!service.Delete(id))
+        Console.WriteLine("Id not found");
 }
